79ce21d86d91c9593fc7bcafa703bf9b3382e199
slince <taosikai@sina.cn>
2015-08-04 17:28:11 +0800

    up

--- src/Route.php
+++ src/Route.php
@@ -1,7 +1,25 @@
 <?php
 namespace Slince\Router;
 
+use Symfony\Component\Routing\Route as SymfonyRoute;
+
 class Route implements RouteInterface
 {
-    
+
+    protected $_uri;
+
+    protected $_parameters;
+
+    protected $_requirements;
+
+    protected $_schemes;
+
+    protected $_methods;
+
+    protected $_domain;
+
+    function compile()
+    {
+        return new SymfonyRoute($this->_uri, $this->_params);
+    }
 }
